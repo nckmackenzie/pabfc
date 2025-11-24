3dbdcca0c29c1a4e1b20d72d126912f728d456e5
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-11-24 13:25:46 +0300

    fix: properly set children

--- src/components/form-components/submit-button.tsx
+++ src/components/form-components/submit-button.tsx
@@ -1,52 +1,52 @@
-import type { ComponentProps } from 'react'
-import { Button } from '@/components/ui/button'
-import { CheckIcon } from '@/components/ui/icons'
-import { LoadingSwap } from '@/components/ui/loading-swap'
-import { useFormContext } from '@/lib/form'
-import { Field } from '../ui/field'
+import type { ComponentProps } from "react";
+import { Button } from "@/components/ui/button";
+import { CheckIcon } from "@/components/ui/icons";
+import { LoadingSwap } from "@/components/ui/loading-swap";
+import { useFormContext } from "@/lib/form";
+import { Field } from "../ui/field";
 
 type SubmitButtonProps = {
-  buttonText: string
-  disabled?: boolean
-  isLoading?: boolean
-  orientation?: 'horizontal' | 'vertical' | 'responsive'
-  withReset?: boolean
-}
+	buttonText: string;
+	disabled?: boolean;
+	isLoading?: boolean;
+	orientation?: "horizontal" | "vertical" | "responsive";
+	withReset?: boolean;
+};
 
 export function SubmitButton({
-  buttonText,
-  disabled,
-  isLoading,
-  orientation,
-  withReset = false,
-}: SubmitButtonProps & ComponentProps<'button'>) {
-  const form = useFormContext()
-  return (
-    <form.Subscribe
-      selector={(state) => [state.isSubmitting]}
-      children={([isSubmitting]) => (
-        <Field orientation={orientation || 'responsive'}>
-          <Button
-            type="submit"
-            disabled={isSubmitting || disabled || isLoading}
-          >
-            <LoadingSwap isLoading={isSubmitting || !!isLoading}>
-              <CheckIcon />
-              {buttonText}
-            </LoadingSwap>
-          </Button>
-          {withReset && (
-            <Button
-              type="button"
-              disabled={isSubmitting}
-              variant="outline"
-              onClick={() => form.reset()}
-            >
-              Cancel
-            </Button>
-          )}
-        </Field>
-      )}
-    />
-  )
+	buttonText,
+	disabled,
+	isLoading,
+	orientation,
+	withReset = false,
+}: SubmitButtonProps & ComponentProps<"button">) {
+	const form = useFormContext();
+	return (
+		<form.Subscribe selector={(state) => [state.isSubmitting]}>
+			{([isSubmitting]) => (
+				<Field orientation={orientation || "responsive"}>
+					<Button
+						type="submit"
+						className="flex"
+						disabled={isSubmitting || disabled || isLoading}
+					>
+						<LoadingSwap isLoading={isSubmitting || !!isLoading}>
+							<CheckIcon />
+							{buttonText}
+						</LoadingSwap>
+					</Button>
+					{withReset && (
+						<Button
+							type="button"
+							disabled={isSubmitting}
+							variant="outline"
+							onClick={() => form.reset()}
+						>
+							Cancel
+						</Button>
+					)}
+				</Field>
+			)}
+		</form.Subscribe>
+	);
 }
