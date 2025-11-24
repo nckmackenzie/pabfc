a23db949bf0aa41bc4bcda609d49574557d4bf51
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-11-24 15:27:31 +0300

    change title from sacco to pabfc

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -36,7 +36,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "Sacco Management",
+				title: "Prime Age Beauty & Fitness Center",
 			},
 		],
 		links: [
