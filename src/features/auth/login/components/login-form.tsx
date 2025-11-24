02d64bfadad4ea0e95d292e26225f9e422770598
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-11-24 19:56:55 +0300

    space elements using field group

--- src/features/auth/login/components/login-form.tsx
+++ src/features/auth/login/components/login-form.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "@tanstack/react-router";
 import toast from "react-hot-toast";
+import { FieldGroup } from "@/components/ui/field";
 import { ToastContent } from "@/components/ui/toast-content";
 import { loginSchema } from "@/features/auth/login/services/schemas";
 import {
@@ -56,25 +57,30 @@ export function LoginForm({ redirectTo }: { redirectTo?: string }) {
 				form.handleSubmit();
 			}}
 		>
-			<form.AppField name="username">
-				{(field) => (
-					<field.Input label="Contact Or Email" placeholder="you@example.com" />
-				)}
-			</form.AppField>
-			<form.AppField name="password">
-				{(field) => (
-					<field.Input isPassword label="Password" placeholder="********" />
-				)}
-			</form.AppField>
-			<Link
-				className="my-4 block text-sm text-right text-primary font-medium transition-all hover:underline"
-				to="/forgot-password"
-			>
-				Forgot Password?
-			</Link>
-			<form.AppForm>
-				<form.SubmitButton buttonText="Login" withReset={false} />
-			</form.AppForm>
+			<FieldGroup>
+				<form.AppField name="username">
+					{(field) => (
+						<field.Input
+							label="Contact Or Email"
+							placeholder="you@example.com"
+						/>
+					)}
+				</form.AppField>
+				<form.AppField name="password">
+					{(field) => (
+						<field.Input isPassword label="Password" placeholder="********" />
+					)}
+				</form.AppField>
+				<Link
+					className="-mt-2 block text-sm text-right text-primary font-medium transition-all hover:underline"
+					to="/forgot-password"
+				>
+					Forgot Password?
+				</Link>
+				<form.AppForm>
+					<form.SubmitButton buttonText="Login" withReset={false} />
+				</form.AppForm>
+			</FieldGroup>
 		</form>
 	);
 }
