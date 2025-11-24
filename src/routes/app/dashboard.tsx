006eba6ff939eb669c4e1f46fba9746c67f05da6
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-11-24 16:42:27 +0300

    initial dashboard

--- src/routes/app/dashboard.tsx
+++ src/routes/app/dashboard.tsx
@@ -1,12 +1,26 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth/client";
 
 export const Route = createFileRoute("/app/dashboard")({
 	head: () => ({
-		meta: [{ title: "Dashboard / Sacco Management" }],
+		meta: [{ title: "Dashboard / Prime Age Beauty & Fitness Center" }],
 	}),
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Dashboard</div>;
+	const router = useRouter();
+	async function handleLogout() {
+		// setOpenMobile(!openMobile)
+		await authClient.signOut();
+		router.navigate({ to: "/sign-in" });
+		router.invalidate();
+		// await queryClient.invalidateQueries()
+	}
+	return (
+		<div>
+			<Button onClick={handleLogout}>signout</Button>
+		</div>
+	);
 }
