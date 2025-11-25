bcd9723651b1f53122fe90856700fcbd8921b404
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-11-25 14:47:21 +0300

    fix: add type import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { Option } from "@/types/index.types";
+import type { Option } from "@/types/index.types";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
