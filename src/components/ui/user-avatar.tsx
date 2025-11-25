7b1f9c9c4438d7a2887aba5f56fde022c9fcd70d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-11-25 05:29:16 +0300

    user avatar component

---
+++ src/components/ui/user-avatar.tsx
@@ -0,0 +1,89 @@
+import { initials } from "@dicebear/collection";
+import { createAvatar } from "@dicebear/core";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "@tanstack/react-router";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	BellIcon,
+	CogIcon,
+	LogOutIcon,
+	UsersIcon,
+} from "@/components/ui/icons";
+import { Skeleton } from "@/components/ui/skeleton";
+import { authClient, useSession } from "@/lib/auth/client";
+
+export function UserAvatar() {
+	const { data: session, isPending } = useSession();
+	const router = useRouter();
+	const queryClient = useQueryClient();
+	if (isPending) {
+		return <Skeleton className="size-8 bg-secondary" />;
+	}
+
+	const avatar = createAvatar(initials, {
+		seed: session?.user.name,
+	});
+
+	async function handleLogout() {
+		await authClient.signOut();
+		router.navigate({ to: "/sign-in" });
+		router.invalidate();
+		await queryClient.invalidateQueries();
+	}
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant="ghost"
+					size="icon"
+					className="hover:bg-transparent focus-visible:ring-[1px]!"
+				>
+					<Avatar>
+						<AvatarImage
+							src={session?.user.image ?? avatar.toDataUri()}
+							alt={session?.user.name ?? "User"}
+						/>
+						<AvatarFallback>
+							{session?.user.name?.slice(0, 2).toUpperCase()}
+						</AvatarFallback>
+					</Avatar>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56" align="start">
+				<DropdownMenuLabel>My Account</DropdownMenuLabel>
+				<DropdownMenuGroup>
+					<DropdownMenuItem className="cursor-pointer">
+						<UsersIcon />
+						Your Profile
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<BellIcon />
+						Notifications
+					</DropdownMenuItem>
+					{session?.user.role === "admin" && (
+						<DropdownMenuItem>
+							<CogIcon />
+							Settings
+						</DropdownMenuItem>
+					)}
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem onSelect={handleLogout}>
+					<LogOutIcon />
+					Log out
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
