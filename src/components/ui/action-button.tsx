d04d7adabc0d806c74924ed4dae6c242cf095703
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-12-03 15:19:40 +0300

    add toast messages

--- src/components/ui/action-button.tsx
+++ src/components/ui/action-button.tsx
@@ -1,5 +1,5 @@
 import { type ComponentProps, type ReactNode, useTransition } from "react";
-import { toast } from "sonner";
+import toast from "react-hot-toast";
 import {
 	AlertDialog,
 	AlertDialogAction,
@@ -13,15 +13,19 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { LoadingSwap } from "@/components/ui/loading-swap";
+import { cn } from "@/lib/utils";
+import { ToastContent } from "./toast-content";
 
 export function ActionButton({
 	action,
 	requireAreYouSure = false,
 	areYouSureDescription = "This action cannot be undone.",
+	isDestructive = true,
 	...props
 }: ComponentProps<typeof Button> & {
 	action: () => Promise<{ error: boolean; message?: string }>;
 	requireAreYouSure?: boolean;
+	isDestructive?: boolean;
 	areYouSureDescription?: ReactNode;
 }) {
 	const [isLoading, startTransition] = useTransition();
@@ -29,7 +33,19 @@ export function ActionButton({
 	function performAction() {
 		startTransition(async () => {
 			const data = await action();
-			if (data.error) toast.error(data.message ?? "Error");
+			if (data.error) {
+				toast.error((t) => (
+					<ToastContent t={t} title="Error" message={data.message || "Error"} />
+				));
+				return;
+			}
+			toast.success((t) => (
+				<ToastContent
+					t={t}
+					title="Action completed successfully!"
+					message={data.message || "Success"}
+				/>
+			));
 		});
 	}
 
@@ -48,7 +64,14 @@ export function ActionButton({
 					</AlertDialogHeader>
 					<AlertDialogFooter>
 						<AlertDialogCancel>Cancel</AlertDialogCancel>
-						<AlertDialogAction disabled={isLoading} onClick={performAction}>
+						<AlertDialogAction
+							disabled={isLoading}
+							onClick={performAction}
+							className={cn(
+								"",
+								isDestructive ? "bg-red-500 hover:bg-red-600" : "",
+							)}
+						>
 							<LoadingSwap isLoading={isLoading}>Yes</LoadingSwap>
 						</AlertDialogAction>
 					</AlertDialogFooter>
