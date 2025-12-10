7aed8d23304b7f0af7ea12f38e5d3103d9d6fe63
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-12-10 16:54:39 +0300

    add error boundary

--- src/routes/app/payments/route.tsx
+++ src/routes/app/payments/route.tsx
@@ -1,7 +1,11 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { AlertErrorComponent } from "@/components/ui/error-component";
 
 export const Route = createFileRoute("/app/payments")({
 	component: RouteComponent,
+	errorComponent: ({ error }) => (
+		<AlertErrorComponent message={error.message} />
+	),
 });
 
 function RouteComponent() {
