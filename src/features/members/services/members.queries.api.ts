d5e662b04ecccc154589931523a1d5bd22a94957
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-12-10 16:54:31 +0300

    fetch last member plan

--- src/features/members/services/members.queries.api.ts
+++ src/features/members/services/members.queries.api.ts
@@ -1,7 +1,7 @@
 import { createServerFn } from "@tanstack/react-start";
 import { and, desc, eq, ilike, ne, or, type SQL, sql } from "drizzle-orm";
 import { db } from "@/drizzle/db";
-import { members, membersOverview } from "@/drizzle/schema";
+import { memberMemberships, members, membersOverview } from "@/drizzle/schema";
 import { memberValidateSearch } from "@/features/members/services/schemas";
 import { authMiddleware } from "@/middlewares/auth-middleware";
 
@@ -101,6 +101,17 @@ export const getMember = createServerFn()
 		return db.query.members.findFirst({
 			columns: { createdAt: false, updatedAt: false },
 			where: eq(members.id, memberId),
+		});
+	});
+
+export const getMemberPreviousPlanDetails = createServerFn()
+	.inputValidator((data: string) => data)
+	.handler(async ({ data: memberId }) => {
+		return db.query.memberMemberships.findFirst({
+			columns: { createdAt: false, updatedAt: false },
+			with: { membershipPlan: { columns: { name: true } } },
+			where: eq(memberMemberships.memberId, memberId),
+			orderBy: desc(memberMemberships.endDate),
 		});
 	});
 
