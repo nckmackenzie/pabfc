452bc25380b6b9477b627d6c43d4038a79d308ae
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-12-11 14:07:05 +0300

    add loading component

--- src/routes/app/payments/new.tsx
+++ src/routes/app/payments/new.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { FormLoader } from "@/components/ui/loaders";
 import { ProtectedPageWithWrapper } from "@/components/ui/protected-page-with-wrapper";
 import { memberQueries } from "@/features/members/services/queries";
 import { PaymentForm } from "@/features/payments/components/payments-form";
@@ -10,17 +11,20 @@ export const Route = createFileRoute("/app/payments/new")({
 	head: () => ({
 		meta: [{ title: "New Payment / Prime Age Beauty & Fitness Club" }],
 	}),
+	pendingComponent: FormLoader,
 	loader: async ({ context: { queryClient } }) => {
 		const [members, plans] = await Promise.all([
 			queryClient.ensureQueryData(memberQueries.activeMembers()),
-			queryClient.ensureQueryData(planQueries.active()),
+			queryClient.ensureQueryData(planQueries.list()),
 		]);
 		return {
 			members: members.map(({ id, fullName }) => ({
 				value: id,
 				label: toTitleCase(fullName),
 			})),
-			plans,
+			plans: plans
+				.filter(({ active }) => active)
+				.map((plan) => ({ ...plan, name: toTitleCase(plan.name) })),
 		};
 	},
 });
