97638163b58fc870165e5d96f955b70740a618d9
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-01-16 17:00:19 +0300

    route start

---
+++ src/routes/app/communication/index.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { ProtectedPage } from "@/components/ui/protected-page";
+import { Wrapper } from "@/components/ui/wrapper";
+
+export const Route = createFileRoute("/app/communication/")({
+	component: RouteComponent,
+	head: () => ({
+		meta: [{ title: "Communication / Prime Age Beauty & Fitness Center" }],
+	}),
+});
+
+function RouteComponent() {
+	return (
+		<ProtectedPage permissions={["communication:view"]}>
+			<div className="grid md:grid-cols-3 gap-4">
+				<Wrapper className="col-span-2">message area</Wrapper>
+
+				<Wrapper>templates area</Wrapper>
+			</div>
+		</ProtectedPage>
+	);
+}
