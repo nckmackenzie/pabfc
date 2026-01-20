10524e4a0739fc9e36d73d97d2420f85ccc39eef
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-01-20 11:40:40 +0300

    return member data along other data

--- src/features/communication/hooks/use-get-form-data.ts
+++ src/features/communication/hooks/use-get-form-data.ts
@@ -2,17 +2,28 @@ import { useQueries } from "@tanstack/react-query";
 import { getRouteApi } from "@tanstack/react-router";
 import { useMemo } from "react";
 import { smsTemplateQueries } from "@/features/communication/services/queries";
+import { memberQueries } from "@/features/members/services/queries";
 import { planQueries } from "@/features/plans/services/queries";
 import { toTitleCase } from "@/lib/utils";
 
 export function useFormData() {
-	const { templates: loaderTemplates, plans: loaderPlans } = getRouteApi(
-		"/app/communication/",
-	).useLoaderData();
-	const [{ data: freshTemplates }, { data: freshPlans }] = useQueries({
-		queries: [smsTemplateQueries.list(), planQueries.active()],
+	const {
+		templates: loaderTemplates,
+		plans: loaderPlans,
+		members: loaderMembers,
+	} = getRouteApi("/app/communication/").useLoaderData();
+	const [
+		{ data: freshTemplates },
+		{ data: freshPlans },
+		{ data: freshMembers },
+	] = useQueries({
+		queries: [
+			smsTemplateQueries.list(),
+			planQueries.active(),
+			memberQueries.list({}),
+		],
 	});
-	const { templates, plans } = useMemo(() => {
+	const { templates, plans, members } = useMemo(() => {
 		return {
 			templates: (freshTemplates || loaderTemplates).map((template) => ({
 				value: template.id,
@@ -22,8 +33,19 @@ export function useFormData() {
 				value: plan.value,
 				label: toTitleCase(plan.label.toLowerCase()),
 			})),
+			members: (freshMembers || loaderMembers).map(({ id, fullName }) => ({
+				value: id,
+				label: toTitleCase(fullName.toLowerCase()),
+			})),
 		};
-	}, [loaderTemplates, freshTemplates, loaderPlans, freshPlans]);
+	}, [
+		loaderTemplates,
+		freshTemplates,
+		loaderPlans,
+		freshPlans,
+		loaderMembers,
+		freshMembers,
+	]);
 
-	return { templates, plans, freshTemplates, freshPlans };
+	return { templates, plans, freshTemplates, freshPlans, members };
 }
