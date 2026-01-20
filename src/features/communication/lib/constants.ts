3eee0de45aa3e206992d6ec53437a9a46117d21c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-01-20 13:00:08 +0300

    add values to FILTER_CRITERIA_OPTIONS

--- src/features/communication/lib/constants.ts
+++ src/features/communication/lib/constants.ts
@@ -1,4 +1,6 @@
 export const FILTER_CRITERIA_OPTIONS = [
+	{ label: "All Members", value: "all members" },
+	{ label: "Specific Members", value: "specific members" },
 	{ label: "By Status", value: "by status" },
 	{ label: "By Plan", value: "by plan" },
 ];
