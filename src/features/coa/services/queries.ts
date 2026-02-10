a128ac6aad2da17e4b002b368e40eac23e9ecb3d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-02-10 22:09:05 +0300

    refactor: add query to get accounts by account type and child accounts only

--- src/features/coa/services/queries.ts
+++ src/features/coa/services/queries.ts
@@ -1,5 +1,6 @@
 import { queryOptions } from "@tanstack/react-query";
 import type { z } from "zod";
+import type { AccountType } from "@/drizzle/schema";
 import type { searchValidateSchema } from "@/lib/schema-rules";
 import { toTitleCase } from "@/lib/utils";
 import { getAccount, getAccounts } from "./coa.api";
@@ -29,5 +30,36 @@ export const accountQueries = {
 		queryOptions({
 			queryKey: [...accountQueries.all, "detail", accountId],
 			queryFn: () => getAccount({ data: accountId }),
+		}),
+	activeAccounts: () =>
+		queryOptions({
+			queryKey: [...accountQueries.all, "active-accounts"],
+			queryFn: async () => {
+				const accounts = await getAccounts({ data: {} });
+				return accounts
+					.filter((account) => account.isActive)
+					.map(({ id, name }) => ({
+						value: id.toString(),
+						label: toTitleCase(name),
+					}));
+			},
+		}),
+	activeChildAccountsByAccountType: (accountType: AccountType) =>
+		queryOptions({
+			queryKey: [...accountQueries.all, "active-accounts", accountType],
+			queryFn: async () => {
+				const accounts = await getAccounts({ data: {} });
+				return accounts
+					.filter(
+						(account) =>
+							account.parentId &&
+							account.isActive &&
+							account.type === accountType,
+					)
+					.map(({ id, name }) => ({
+						value: id.toString(),
+						label: toTitleCase(name),
+					}));
+			},
 		}),
 };
