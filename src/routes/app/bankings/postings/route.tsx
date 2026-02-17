dc6dbe3de70cbd12960969ef2239406e52777097
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-02-17 15:55:47 +0300

    refactor: have exact as false in route layout

--- src/routes/app/bankings/postings/route.tsx
+++ src/routes/app/bankings/postings/route.tsx
@@ -1,17 +1,12 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { getBanks } from "@/features/bankings/services/bankings.api";
 import { accountQueries } from "@/features/coa/services/queries";
 import { toTitleCase } from "@/lib/utils";
 
 export const Route = createFileRoute("/app/bankings/postings")({
 	component: RouteComponent,
 	beforeLoad: async ({ context: { queryClient } }) => {
-		const [banks, accounts] = await Promise.all([
-			getBanks(),
-			queryClient.ensureQueryData(accountQueries.list({})),
-		]);
+		const accounts = await queryClient.ensureQueryData(accountQueries.list({}));
 		return {
-			banks,
 			accounts: accounts
 				.filter((a) => a.isActive && a.isPosting)
 				.map(({ id, name }) => ({
