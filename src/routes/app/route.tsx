689aabd7b9e2d163d7e1130b5ad0b1f68030af9d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-02-17 13:00:43 +0300

    remove the spinner

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -2,7 +2,7 @@ import {
 	createFileRoute,
 	Outlet,
 	redirect,
-	useRouterState,
+	// useRouterState,
 } from "@tanstack/react-router";
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { RouterBreadcrumb } from "@/components/ui/nav-breadcrumb";
@@ -14,19 +14,19 @@ import {
 import { UserAvatar } from "@/components/ui/user-avatar";
 import { Wip } from "@/components/ui/wip";
 
-function Spinner({ show, wait }: { show?: boolean; wait?: `delay-${number}` }) {
-	return (
-		<div
-			className={`inline-block animate-spin px-3 transition ${
-				(show ?? true)
-					? `opacity-1 duration-500 ${wait ?? "delay-300"}`
-					: "duration-500 opacity-0 delay-0"
-			}`}
-		>
-			⍥
-		</div>
-	);
-}
+// function Spinner({ show, wait }: { show?: boolean; wait?: `delay-${number}` }) {
+// 	return (
+// 		<div
+// 			className={`inline-block animate-spin px-3 transition ${
+// 				(show ?? true)
+// 					? `opacity-1 duration-500 ${wait ?? "delay-300"}`
+// 					: "duration-500 opacity-0 delay-0"
+// 			}`}
+// 		>
+// 			⍥
+// 		</div>
+// 	);
+// }
 
 export const Route = createFileRoute("/app")({
 	beforeLoad: async ({ context, location }) => {
@@ -39,7 +39,7 @@ export const Route = createFileRoute("/app")({
 });
 
 function RouteComponent() {
-	const isLoading = useRouterState({ select: (s) => s.status === "pending" });
+	// const isLoading = useRouterState({ select: (s) => s.status === "pending" });
 	return (
 		<SidebarProvider>
 			<AppSidebar />
@@ -55,7 +55,7 @@ function RouteComponent() {
 				</header>
 				<div className="flex flex-1 flex-col gap-4 bg-secondary ">
 					<div className="max-w-7xl mx-auto w-full p-4">
-						{isLoading ? <Spinner show={isLoading} /> : <Outlet />}
+						<Outlet />
 					</div>
 				</div>
 			</SidebarInset>
