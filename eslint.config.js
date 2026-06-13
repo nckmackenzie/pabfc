e4dd5652015211303e7c676a2d1139f2b31a4b87
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-06-13 17:21:44 +0300

    add rule for type imports

--- eslint.config.js
+++ eslint.config.js
@@ -45,6 +45,7 @@ export default tseslint.config(
           allowConstantExport: true,
         },
       ],
+      "@typescript-eslint/consistent-type-imports": "error",
 
       "@typescript-eslint/no-unused-vars": [
         "warn",
