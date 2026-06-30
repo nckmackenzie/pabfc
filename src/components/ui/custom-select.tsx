2ee3264d63e6dd524c44d2140f5efba23425fe75
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-06-30 20:58:39 +0300

    remove scroll area

--- src/components/ui/custom-select.tsx
+++ src/components/ui/custom-select.tsx
@@ -10,14 +10,9 @@ import {
 	CommandSeparator,
 } from "@/components/ui/command";
 import { CheckIcon, ChevronUpDownIcon } from "@/components/ui/icons";
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import type { Option } from "@/types/index.types";
-import { ScrollArea } from "./scroll-area";
 
 interface ComboBoxProps {
 	items: Array<Option>;
@@ -51,45 +46,35 @@ export function ComboBox({
 					aria-expanded={open}
 					className={cn(
 						"w-full justify-between h-10 shadow-none!",
-						isInvalid && "border-destructive",
+						isInvalid && "border-destructive"
 					)}
 					disabled={disabled}
 				>
-					{value
-						? items.find((item) => item.value === value)?.label
-						: placeholder}
+					{value ? items.find((item) => item.value === value)?.label : placeholder}
 					<ChevronUpDownIcon className="opacity-50" />
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className="max-w-md p-0">
 				<Command>
-					<CommandInput
-						placeholder={commandPlaceholder ?? placeholder}
-						className="h-10 "
-					/>
+					<CommandInput placeholder={commandPlaceholder ?? placeholder} className="h-10 " />
 					<CommandList>
 						<CommandEmpty>No options found.</CommandEmpty>
 						<CommandGroup>
-							<ScrollArea className="max-h-[300px] [&>div]:block!">
-								{items.map((item) => (
-									<CommandItem
-										key={item.value}
-										value={item.label}
-										onSelect={() => {
-											onChange(item.value === value ? "" : item.value);
-											setOpen(false);
-										}}
-									>
-										{item.label}
-										<CheckIcon
-											className={cn(
-												"ml-auto",
-												value === item.value ? "opacity-100" : "opacity-0",
-											)}
-										/>
-									</CommandItem>
-								))}
-							</ScrollArea>
+							{items.map((item) => (
+								<CommandItem
+									key={item.value}
+									value={item.label}
+									onSelect={() => {
+										onChange(item.value === value ? "" : item.value);
+										setOpen(false);
+									}}
+								>
+									{item.label}
+									<CheckIcon
+										className={cn("ml-auto", value === item.value ? "opacity-100" : "opacity-0")}
+									/>
+								</CommandItem>
+							))}
 						</CommandGroup>
 					</CommandList>
 					{addNew && (
